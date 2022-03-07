9f328d29b60fc2511fe37856010fa7fb55530daf
zenter <46599188+zkozak34@users.noreply.github.com>
2022-03-08 01:00:43 +0300

    Add choose customer and add new customer functions

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ConsoleTables;
 
 namespace ECommerce
 {
@@ -11,55 +12,134 @@ namespace ECommerce
             // Variables
             int appSection = 1;
             string[] operations = {"Sipariş ekle", "Siparişleri listele", "Müşteri bilgilerini göster"};
+            Customer selectedCustomer = null;
             // Dummy Data
-            List<Customer> customers = new List<Customer>() {
+            List<Customer> customers = new List<Customer>()
+            {
                 new Customer
                 {
-                    Id = Guid.NewGuid(), TcNo = "12345678901", Fullname = "Zeynel KOZAK", Gsm = "5435434343",
+                    Id = Guid.NewGuid().ToString().Substring(0, 8), TcNo = "12345678901", Fullname = "Zeynel KOZAK",
+                    Gsm = "5435434343",
                     Address = "Istanbul"
                 },
                 new Customer
                 {
-                    Id = Guid.NewGuid(), TcNo = "12345678902", Fullname = "Ahmet Bey", Gsm = "5325323232",
+                    Id = Guid.NewGuid().ToString().Substring(0, 8), TcNo = "12345678902", Fullname = "Ahmet Bey",
+                    Gsm = "5325323232",
                     Address = "Ankara"
                 },
                 new Customer
                 {
-                    Id = Guid.NewGuid(), TcNo = "12345678903", Fullname = "Mehmet Bey", Gsm = "5215212121",
+                    Id = Guid.NewGuid().ToString().Substring(0, 8), TcNo = "12345678903", Fullname = "Mehmet Bey",
+                    Gsm = "5215212121",
                     Address = "Konya"
                 },
                 new Customer
                 {
-                    Id = Guid.NewGuid(), TcNo = "12345678904", Fullname = "Ali Bey", Gsm = "5105101010",
+                    Id = Guid.NewGuid().ToString().Substring(0, 8), TcNo = "12345678904", Fullname = "Ali Bey",
+                    Gsm = "5105101010",
                     Address = "Eskişehir"
                 },
             };
-            List<Product> products = new List<Product>() {
+            List<Product> products = new List<Product>()
+            {
                 new Product {Id = Guid.NewGuid(), Name = "Product 1", Price = 100, Stock = 10},
                 new Product {Id = Guid.NewGuid(), Name = "Product 2", Price = 200, Stock = 12},
                 new Product {Id = Guid.NewGuid(), Name = "Product 3", Price = 300, Stock = 14},
                 new Product {Id = Guid.NewGuid(), Name = "Product 4", Price = 400, Stock = 16},
                 new Product {Id = Guid.NewGuid(), Name = "Product 5", Price = 500, Stock = 18}
             };
-            List<Order> orders = new List<Order>() {
-                new Order() {Id = Guid.NewGuid(), CustomerId = customers[0].Id, Products = {products[0], products[3]}},
-                new Order() {Id = Guid.NewGuid(), CustomerId = customers[1].Id, Products = {products[1], products[2]}},
-                new Order() {Id = Guid.NewGuid(), CustomerId = customers[2].Id, Products = {products[2], products[3]}},
-                new Order() {Id = Guid.NewGuid(), CustomerId = customers[3].Id, Products = {products[2], products[4]}},
-            };
-            
+
+            Console.WriteLine("# Müşteri Sipariş Hattı");
+            // App Section 1 - Choose customer or add new customer
+            while (appSection == 1)
+            {
+                Console.WriteLine("1. Yeni müşteri oluştur");
+                Console.WriteLine("2. Mevcut müşteri üzerinden işlem yap");
+                Console.Write("İşlem seçiniz: ");
+                string op = Console.ReadLine();
+                if (op == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Müşteri oluştur.");
+                    AddNewCustomer();
+                }
+                else if (op == "2")
+                {
+                    Console.Clear();
+                    GetCustomers();
+                    Console.Write("Müşteri seç: ");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c == 0 || c > customers.Count)
+                    {
+                        Console.WriteLine("Listeden müşteri seçiniz.");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        selectedCustomer = customers[c - 1];
+                        Console.WriteLine("Seçilen müşteri: {0}", selectedCustomer.Fullname);
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Lütfen bir işlem seçiniz.");
+                }
+            }
+
+            // Functions
             Customer GetCustomer(string tcNo)
             {
                 return customers.SingleOrDefault(c => c.TcNo == tcNo);
+            }
+
+            void GetCustomers()
+            {
+                customers = customers.ToList();
+                var table = new ConsoleTable("#", "Ad ve Soyad", "T.C. No.", "Telefon Numarası", "Adres");
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    table.AddRow(i + 1, customers[i].Fullname, customers[i].TcNo, customers[i].Gsm,
+                        customers[i].Address);
+                }
+
+                table.Write();
+            }
+
+            void AddNewCustomer()
+            {
+                Console.Write("T.C. No.: ");
+                string tcNo = Console.ReadLine();
+                Console.Write("Ad Soyad: ");
+                string fullName = Console.ReadLine();
+                Console.Write("Telefon Numarası: ");
+                string gsm = Console.ReadLine();
+                Console.Write("Adres: ");
+                string address = Console.ReadLine();
+
+                if (tcNo == "" || fullName == "" || gsm == "" || address == "")
+                {
+                    Console.WriteLine("Lütfen istenilen bilgileri eksiksiz giriniz.");
+                }
+                else
+                {
+                    customers.Add(new Customer()
+                    {
+                        Id = Guid.NewGuid().ToString().Substring(0, 8), Fullname = fullName, Gsm = gsm,
+                        Address = address, TcNo = tcNo
+                    });
+                    Console.WriteLine("Yeni müşteri oluşturuldu.");
+                }
             }
         }
     }
 
     class Customer
     {
-        public Guid Id { get; set; }
-        public string TcNo { get; set; }
+        public string Id { get; set; }
         public string Fullname { get; set; }
+        public string TcNo { get; set; }
         public string Gsm { get; set; }
         public string Address { get; set; }
     }
@@ -77,6 +157,6 @@ namespace ECommerce
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
         public List<Product> Products { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; }
     }
 }
